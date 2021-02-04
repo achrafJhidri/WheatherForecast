8a053d576373851ec14106ac63910b936f93842b
achraf jhidri <achrafjh@gmail.com>
2021-02-04 22:16:47 +0100

    first attempt of wheather api

---
+++ src/api/wheatherApi.js
@@ -0,0 +1,25 @@
+const ApiKey = "17e330397fee8a55928ef4dcba378607"
+const oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall"
+const imageUrl = " http://openweathermap.org/img/w/ de +.png"
+
+
+export const getWheatherByCoords = async ({longitude,latitude}) => {
+  return fetchFunction(oneCallUrl+"?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+ApiKey)
+} 
+  
+fetchFunction = async (url ) => {
+    try {
+  
+      const response = await fetch(
+        url
+      );
+      const json = await response.json();
+
+      return json;
+    } catch (error) {
+
+      throw error;
+    }
+  }
+  
+  
