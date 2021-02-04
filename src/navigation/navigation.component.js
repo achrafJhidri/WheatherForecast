81463b690ad76b8cd1ba9bc56ccd5bcb73ece503
achraf jhidri <achrafjh@gmail.com>
2021-02-04 22:17:34 +0100

    navigation

---
+++ src/navigation/navigation.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../components/Home.component';
+import { SearchScreen } from '../components/Search.component';
+import {FavoritsScreen} from '../components/Favorits.component'
+
+const { Navigator, Screen } = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Navigator headerMode='none'>
+    <Screen name='Home' component={HomeScreen}/>
+    <Screen name='Favorits' component={FavoritsScreen}/>
+    <Screen name='Search' component={SearchScreen}/>
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <HomeNavigator/>
+  </NavigationContainer>
+);
