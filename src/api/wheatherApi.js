69df7c5fa23501e894b3235daf83764c8244dd4d
achraf jhidri <achrafjh@gmail.com>
2021-02-05 21:17:50 +0100

    getWheatherByCityName

--- src/api/wheatherApi.js
+++ src/api/wheatherApi.js
@@ -1,11 +1,16 @@
 const ApiKey = "17e330397fee8a55928ef4dcba378607"
 const oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall"
+const currentWheather = "https://api.openweathermap.org/data/2.5/weather?"
 const imageUrl = " http://openweathermap.org/img/w/ de +.png"
 
 
 export const getWheatherByCoords = async ({longitude,latitude}) => {
   return fetchFunction(oneCallUrl+"?lat="+latitude+"&lon="+longitude+"&units=metric&appid="+ApiKey)
 } 
+
+export const getWheatherByCityName = async (cityName) =>{
+  return fetchFunction(currentWheather+`q=${cityName}&appid=${ApiKey}`)
+}
   
 fetchFunction = async (url ) => {
     try {
